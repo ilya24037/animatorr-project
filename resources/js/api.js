228f3cb4d634ec4611d80a05b33ad5d7613feb69
ilya24037 <ilya24037@gmail.com>
2025-05-21 21:32:40 +0500

    1

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: '/api/v1',
   withCredentials: true,
-});
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+})
+
+// здесь могут быть interceptors…
+
+export default api   // ← теперь доступен default‑export
